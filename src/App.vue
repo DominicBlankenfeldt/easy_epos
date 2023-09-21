c6130d1aaa18de5cc180c744230fa0b8115611c6
DominicBlankenfeldt <88446843+DominicBlankenfeldt@users.noreply.github.com>
2023-09-21 22:35:39 +0200

    design der login-page

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Arial, Helvetica, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,6 +1,42 @@
 <template>
-  <div class="login">
-    <div class=""></div>
+  <div class="login d-flex justify-content-center">
+    <div class="ccard w-50">
+      <div class="ccard-header pt-5">
+        <div><h4>easyepos</h4></div>
+        <div><h2>Login</h2></div>
+      </div>
+      <div class="ccard-body">
+        <div class="login-form">
+          <div class="m-3 d-flex flex-column justify-content-start">
+            <label for="username">Username</label>
+            <input
+              class="cborder-light border-round"
+              type="text"
+              placeholder=""
+              name="username"
+              required
+            />
+          </div>
+          <div class="m-3 d-flex flex-column justify-content-start">
+            <label for="psw">Password</label>
+            <input
+              class="cborder-light border-round"
+              type="password"
+              placeholder=""
+              name="psw"
+              required
+            />
+          </div>
+
+          <div class="m-3 cbtn cbtn-yellow mt-5">Login</div>
+          <label>
+            <input type="checkbox" checked="checked" name="remember" />
+            Remember me
+          </label>
+        </div>
+      </div>
+      <div class="ccard-footer"></div>
+    </div>
   </div>
 </template>
 
@@ -13,4 +49,16 @@ export default defineComponent({
 });
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.login {
+  padding-top: 18%;
+}
+.login-form {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+input {
+  text-align: center;
+}
+</style>
